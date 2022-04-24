73854e1b21ec80015efbd9f5e9f350411e0ebb7e
Egor <goldenyakegor@gmail.com>
2022-04-24 21:32:34 +0300

    did some editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import {Header} from "./components/Header/Header";
+import {InitialPage} from "./pages/InitialPage/InitialPage";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 export const App = () => {
     return (
         <div className="App">
-          <Header/>
+            <Provider store={store}>
+                <Routes>
+                    <Route path="/" element={<InitialPage/>}/>
+                </Routes>
+            </Provider>
+
         </div>
-    );
+);
 }
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import s from "./Search.module.css";
 import iconSearch from "../../images/iconSearch.png";
+import {useDispatch} from "react-redux";
+import {fetchReposTC} from "../../redux/repos-reducer";
+import {fetchUserTC, setInputValueAC} from "../../redux/user-reducer";
 
 export const Search = () => {
+
+    const [value, setValue] = useState('');
+    const [error, setError] = useState<string | null>(null);
+
+    const dispatch = useDispatch();
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    };
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null);
+        }
+        if (e.charCode === 13) {
+            addInputValueHandler();
+        }
+    };
+
+    const addInputValueHandler = () => {
+        if (value.trim() !== '') {
+            // dispatch(setInputValueAC(value))
+            // @ts-ignore
+            dispatch(fetchUserTC(value))
+            // dispatch(fetchReposTC(value))
+            setValue('');
+        } else {
+            setError('Поле не может быть пустым');
+        }
+    };
+
+
     return (
-        <label className={s.label}>
-            <img className={s.iconSearch} src={iconSearch} alt='search-icon'/>
-            <input className={s.input} type='search' placeholder='Enter GitHub username'/>
-        </label>
+        <>
+            <label className={s.label}>
+                <img className={s.iconSearch} src={iconSearch} alt='search-icon'/>
+                <input className={s.input}
+                       type='search'
+                       placeholder='Enter GitHub username'
+                       value={value}
+                       onChange={(e) => handleInput(e)}
+                       onKeyPress={onKeyPressHandler}/>
+
+            </label>
+            {error ? <div style={{color: 'red', fontSize: '12px'}}>{error}</div> : null}
+        </>
+
     );
 };
 
